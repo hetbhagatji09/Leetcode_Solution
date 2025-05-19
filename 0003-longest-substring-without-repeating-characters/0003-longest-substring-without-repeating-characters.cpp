8996c2c714750b4eab6f017156b513ed55a27b5f
Het  YogeshKumar Bhagatji <152509283+hetbhagatji09@users.noreply.github.com>
2025-05-19 19:10:09 +0530

    Time: 173 ms (8.98%), Space: 16.7 MB (13.19%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -7,15 +7,16 @@ public:
         int maxLength=0;
         int n=s.size();
         while(r<n){
-            if(umap.find(s[r])!=umap.end()){
+            if(umap.find(s[r])!=umap.end() && umap[s[r]]>=l){
                 auto value=umap[s[r]];
-                l=max(l,value+1);
+                l=value+1;
                 umap[s[r]]=r;
             }
             
-                umap[s[r]]=r;
+                
                 maxLength=max(maxLength,r-l+1);
                 cout<<s.substr(l,r-l+1)<<" ";
+                umap[s[r]]=r;
                 
             
             r++;

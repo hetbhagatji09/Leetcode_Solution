fb0ba6c7166e8c543c3b0c55190c1ba2ed7eaaa6
Het  YogeshKumar Bhagatji <152509283+hetbhagatji09@users.noreply.github.com>
2025-06-08 01:32:53 +0530

    Time: 0 ms (100%), Space: 10.1 MB (89.83%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -9,8 +9,16 @@ public:
         return dp[ind]=max(notpick,pick);
     }
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>dp(n,-1);
-        return f(n-1,nums,dp);
+        int prev=nums[0];
+        int prev2=0;
+        for(int i=0;i<nums.size();i++){
+            int take=nums[i];
+            if(i>1) take+=prev2;
+            int nottake=0 + prev;
+            int current=max(take,nottake);
+            prev2=prev;
+            prev=current;
+        }
+        return prev;
     }
 };

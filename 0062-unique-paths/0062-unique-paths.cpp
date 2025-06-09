37927bcb4a27e2f785458485a6da52b642ca2cb5
Het  YogeshKumar Bhagatji <152509283+hetbhagatji09@users.noreply.github.com>
2025-06-09 15:49:01 +0530

    Time: 0 ms (100%), Space: 9.2 MB (56.87%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -10,6 +10,26 @@ public:
     }
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return f(m-1,n-1,dp);
+        // return f(m-1,n-1,dp);
+        
+        for(int i=0;i<m;i++){
+            int left=0,up=0;
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[0][0]=1;
+                    continue;
+                }
+                else{
+                    int left=0,right=0;
+                    if(j>0)
+                    left=dp[i][j-1];
+                    if(i>0)
+                    up=dp[i-1][j];
+                    dp[i][j]=left+up;
+                }
+                
+            }
+        }
+        return dp[m-1][n-1];
     }
 };

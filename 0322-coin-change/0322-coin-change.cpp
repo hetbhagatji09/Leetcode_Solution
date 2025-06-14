9f367344b87214b929d7b84477d3c8582cdaf42a
Het  YogeshKumar Bhagatji <152509283+hetbhagatji09@users.noreply.github.com>
2025-06-14 23:07:52 +0530

    Time: 42 ms (43.95%), Space: 57.3 MB (7.18%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -16,20 +16,22 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        vector<int>prev(amount+1,0),curr(amount+1,0);
         for(int target=0;target<=amount;target++){
-            if(target % coins[0] == 0) dp[0][target]=(target/coins[0]);
-            else dp[0][target]=1e9;
+            if(target % coins[0] == 0) prev[target]=(target/coins[0]);
+            else prev[target]=1e9;
         }
         for(int i=1;i<n;i++){
             for(int t=0;t<=amount;t++){
-                int not_take=0 + dp[i-1][t];
+                int not_take=0 + prev[t];
                 int take=1e9;
-                if(coins[i]<=t) take=1 + dp[i][t-coins[i]];
-                dp[i][t]=min(take,not_take);
+                if(coins[i]<=t) take=1 + curr[t-coins[i]];
+                curr[t]=min(take,not_take);
             }
+            prev=curr;
         }
-        if(dp[n-1][amount] >= 1e9) return -1;
-        return dp[n-1][amount];
+        if(prev[amount] >= 1e9) return -1;
+        return prev[amount];
         // int ans=f(n-1,amount,coins,dp);
         // if(ans >=1e9) return -1;
         // return ans; 

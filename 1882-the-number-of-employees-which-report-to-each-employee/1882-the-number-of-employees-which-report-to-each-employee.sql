476adeaf1f7f8c4453ccb8d9111ec263973d5783
Het  YogeshKumar Bhagatji <152509283+hetbhagatji09@users.noreply.github.com>
2025-07-14 16:38:28 +0530

    Time: 558 ms (97.02%), Space: 0B (100%) - LeetHub

--- 1882-the-number-of-employees-which-report-to-each-employee/1882-the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/1882-the-number-of-employees-which-report-to-each-employee.sql
@@ -5,5 +5,6 @@ SELECT
     ROUND(AVG(e.age), 0) AS average_age
 FROM employees e
 JOIN employees m ON e.reports_to = m.employee_id
-GROUP BY m.employee_id, m.name
-ORDER BY m.employee_id;
+GROUP BY m.employee_id
+order by m.employee_id
+
